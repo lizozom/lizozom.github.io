0cfecadcae2a4355d9354d346b84324c2af91cab
lizozom <lizka.k@gmail.com>
2024-06-12 20:32:52 +0300

    Updates

---
+++ component---src-pages-404-jsx-dbdadc73f05a66536b28.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdevfolio=self.webpackChunkdevfolio||[]).push([[415],{3729:function(e,t,l){l.r(t);var n=l(6540),o=l(4794),a=l(8776),u=l(122);const s="text-lg font-bold",c="underline";t.default=()=>n.createElement(a.A,null,n.createElement(u.A,{title:"Not found"}),n.createElement("h1",{className:s},"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist."," ",n.createElement(o.Link,{className:c,to:"/"},"Return to safety"),"."))}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-dbdadc73f05a66536b28.js.map
