158e54d46fedbde26e02d9a990198df724a750df
lizozom <lizka.k@gmail.com>
2024-06-12 20:53:25 +0300

    open links in new tab

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -10,7 +10,7 @@ const classes = {
 const SummaryItem = ({ name, description, link = false, internal = false }) => {
   let linkContent;
   if (internal) {
-    linkContent = <Link to={link}>{name}</Link>;
+    linkContent = <Link to={link} target='_blank'>{name}</Link>;
   } else {
     linkContent = <a href={link} target="_blank">{name}</a>;
   }
